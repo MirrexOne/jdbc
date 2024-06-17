0799fb7a2ced691dc193a157b94978bf9de92e3d
Alex Belovitsky <mirrexone@gmail.com>
2024-06-17 12:33:43 +0300

    Update statements try-with-resources

--- src/main/java/mirrex/dev/Application.java
+++ src/main/java/mirrex/dev/Application.java
@@ -6,30 +6,34 @@ import java.sql.SQLException;
 public class Application {
     public static void main(String[] args) throws SQLException {
 
-        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet")) {
+            var sql = "CREATE TABLE films" + "(id BIGINT PRIMARY KEY AUTO_INCREMENT,"
+                    + " title VARCHAR(255), release_year INT, duration INT)";
 
-        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            try (var statement = conn.createStatement()) {
+                statement.execute(sql);
 
-        var statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close();
+            }
 
-        var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
+            String query = "INSERT INTO films (title, release_year, duration) VALUES "
+                    + "('Godfather', 175, 1972),"
+                    + "('The Green Mile', 189, 1999)";
 
-        var sql3 = "SELECT * FROM users";
-        var statement3 = conn.createStatement();
+            try (var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(query);
+            }
 
-        var resultSet = statement3.executeQuery(sql3);
+            var dataSelection = "SELECT * FROM films";
+            try (var statement3 = conn.createStatement()) {
+                var resultSet = statement3.executeQuery(dataSelection);
+                while (resultSet.next()) {
+                    System.out.printf("%s %s %s\n",
+                            resultSet.getString("title"),
+                            resultSet.getInt("release_year"),
+                            resultSet.getInt("duration"));
 
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
+                }
+            }
         }
-        statement3.close();
-
-        conn.close();
     }
 }
